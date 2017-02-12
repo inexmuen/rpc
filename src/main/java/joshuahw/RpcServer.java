53a02ecd45d9a64955a3a1bf121d874f593d3e4c
Joshua <517081935@qq.com>
2017-02-12 23:13:49 +0800

    add server, client, test code

--- src/main/java/joshuahw/RpcServer.java
+++ src/main/java/joshuahw/RpcServer.java
@@ -1,8 +1,18 @@
 package joshuahw;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.apache.commons.collections4.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
@@ -12,7 +22,7 @@ import java.util.Map;
 /**
  * 启动并注册服务
  */
-public class RpcServer implements ApplicationContextAware {
+public class RpcServer implements ApplicationContextAware, InitializingBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RpcServer.class);
 
@@ -31,7 +41,50 @@ public class RpcServer implements ApplicationContextAware {
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        Map<String, Object> serviceBeanMap = ctx.getBeansWithAnnotation(RpcService.class); // 获取所有带有 RpcService 注解的 Spring Bean
+        if (MapUtils.isNotEmpty(serviceBeanMap)) {
+            for (Object serviceBean : serviceBeanMap.values()) {
+                String interfaceName = serviceBean.getClass().getAnnotation(RpcService.class).value().getName();
+                handlerMap.put(interfaceName, serviceBean);
+            }
+        }
+    }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel channel) throws Exception {
+                            channel.pipeline()
+                                    .addLast(new RpcDecoder(RpcRequest.class)) // 将 RPC 请求进行解码（为了处理请求）
+                                    .addLast(new RpcEncoder(RpcResponse.class)) // 将 RPC 响应进行编码（为了返回响应）
+                                    .addLast(new RpcHandler(handlerMap)); // 处理 RPC 请求
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            String[] array = serverAddress.split(":");
+            String host = array[0];
+            int port = Integer.parseInt(array[1]);
+
+            ChannelFuture future = bootstrap.bind(host, port).sync();
+            LOGGER.debug("server started on port {}", port);
+
+            if (serviceRegistry != null) {
+                serviceRegistry.register(serverAddress); // 注册服务地址
+            }
+
+            future.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
     }
 }
---
+++ src/test/java/joshuahw/rpc/test/HelloServiceTest.java
@@ -0,0 +1,26 @@
+package joshuahw.rpc.test;
+
+import joshuahw.HelloService;
+import joshuahw.RpcProxy;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-zk-rpc-server.xml")
+public class HelloServiceTest {
+
+    @Autowired
+    private RpcProxy rpcProxy;
+
+    @Test
+    public void helloTest() {
+        HelloService helloService = rpcProxy.create(HelloService.class);
+        String result = helloService.hello("World");
+        Assert.assertEquals("Hello! World", result);
+    }
+}
+
