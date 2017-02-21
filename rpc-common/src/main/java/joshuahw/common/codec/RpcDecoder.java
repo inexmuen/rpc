4526f497c68cd595132125c853aafc02f1ee5834
Joshua <517081935@qq.com>
2017-02-21 22:34:04 +0800

    add rpc-common

--- rpc-common/src/main/java/joshuahw/common/codec/RpcDecoder.java
+++ rpc-common/src/main/java/joshuahw/common/codec/RpcDecoder.java
@@ -1,7 +1,36 @@
 package joshuahw.common.codec;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import joshuahw.common.util.SerializationUtil;
+
+import java.util.List;
+
 /**
- * Created by joshua on 17/2/21.
+ * RPC 解码
  */
-public class RpcDecoder {
+public class RpcDecoder extends ByteToMessageDecoder {
+
+    private Class<?> genericClass;
+
+    public RpcDecoder(Class<?> genericClass) {
+        this.genericClass = genericClass;
+    }
+
+    @Override
+    public void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        if (in.readableBytes() < 4) {
+            return;
+        }
+        in.markReaderIndex();
+        int dataLength = in.readInt();
+        if (in.readableBytes() < dataLength) {
+            in.resetReaderIndex();
+            return;
+        }
+        byte[] data = new byte[dataLength];
+        in.readBytes(data);
+        out.add(SerializationUtil.deserialize(data, genericClass));
+    }
 }
--- rpc-common/src/main/java/joshuahw/common/util/CollectionUtil.java
+++ rpc-common/src/main/java/joshuahw/common/util/CollectionUtil.java
@@ -1,7 +1,41 @@
 package joshuahw.common.util;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+
+import java.util.Collection;
+import java.util.Map;
+
 /**
- * Created by joshua on 17/2/21.
+ * 集合工具类
  */
 public class CollectionUtil {
+
+    /**
+     * 判断 Collection 是否为空
+     */
+    public static boolean isEmpty(Collection<?> collection) {
+        return CollectionUtils.isEmpty(collection);
+    }
+
+    /**
+     * 判断 Collection 是否非空
+     */
+    public static boolean isNotEmpty(Collection<?> collection) {
+        return !isEmpty(collection);
+    }
+
+    /**
+     * 判断 Map 是否为空
+     */
+    public static boolean isEmpty(Map<?, ?> map) {
+        return MapUtils.isEmpty(map);
+    }
+
+    /**
+     * 判断 Map 是否非空
+     */
+    public static boolean isNotEmpty(Map<?, ?> map) {
+        return !isEmpty(map);
+    }
 }
